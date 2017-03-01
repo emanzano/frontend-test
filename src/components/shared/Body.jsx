3e5e98802175f3100da07de85f7eb7f095a8792b
Ezequiel Agustín Manzano <ezequiel.manzano@mercadolibre.com.ar>
2017-03-01 09:16:43 -0300

    Added shared Body component

---
+++ src/components/shared/Body.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function List(props) {
+    return (
+        <div className="body">
+
+        </div>
+    );
+}
+
+List.propTypes = {
+    onClickItem: React.PropTypes.func.isRequired,
+    isItemDetail: React.PropTypes.bool.isRequired,
+    items: React.PropTypes.arrayOf(React.PropTypes.shape({
+        id: React.PropTypes.string.isRequired,
+        title: React.PropTypes.string.isRequired,
+        price: {
+            currency: React.PropTypes.string.isRequired,
+            amount: React.PropTypes.number.isRequired,
+            decimals: React.PropTypes.number.isRequired
+        },
+        picture: React.PropTypes.string.isRequired,
+        condition: React.PropTypes.string.isRequired,
+        free_shipping: React.PropTypes.string.isRequired,
+        state: React.PropTypes.bool.isRequired
+    }))
+}
